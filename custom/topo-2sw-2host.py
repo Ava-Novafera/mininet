f5ecc937f737700b950b8067c06f3e8265281847
Ava-Novafera <52157366+Ava-Novafera@users.noreply.github.com>
2021-01-30 22:39:03 +0330

    Update topo-2sw-2host.py

--- custom/topo-2sw-2host.py
+++ custom/topo-2sw-2host.py
@@ -2,7 +2,11 @@
 
 Two directly connected switches plus a host for each switch:
 
-   host --- switch --- switch --- host
+               host
+               |
+               |
+ 
+ host --- switch --- host
 
 Adding the 'topos' dict with a key/value pair to generate our newly defined
 topology enables one to pass in '--topo=mytopo' from the command line.
@@ -17,15 +21,16 @@ class MyTopo( Topo ):
         "Create custom topo."
 
         # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        host1 = self.addHost( 'h1', ip="10.0.1.100/24", defaultRoute = "via 10.0.1.1" )
+        host2 = self.addHost( 'h2', ip="10.0.2.100/24", defaultRoute = "via 10.0.2.1" )
+        host3 = self.addHost( 'h3', ip="10.0.3.100/24", defaultRoute = "via 10.0.3.1" )
+        switch = self.addSwitch( 's1' )
 
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        self.addLink( host1, switch )
+        self.addLink( host2, switch )
+        self.addLink( host3, switch )
+
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
